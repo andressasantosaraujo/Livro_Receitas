622a03e016f1fe67ef228f74d2f7d66fbca095e8
andressasantosaraujo <andressasantosaraujo@users.noreply.github.com>
2016-12-09 16:41:26 -0200

    Add files via upload

---
+++ receita.sql
@@ -0,0 +1,10 @@
+﻿DROP TABLE IF EXISTS receita
+
+CREATE TABLE receita
+(
+  id SERIAL,
+  titulo VARCHAR(30) NOT NULL,
+  ingredientes VARCHAR(100) NOT NULL,
+  passo_passo VARCHAR(500) NOT NULL,
+  PRIMARY KEY (id)
+);
